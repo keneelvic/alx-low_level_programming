60986b2908832a6ed1e99607e13eff8bc23e5b01
Keneel <keneelsoundton@gmail.com>
2022-09-09 02:07:09 -0700

    a script that runs a C file through the preprocessor and save the result into another file.

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * printf prints hello world to stdout
+ *
+ * return - always 0 signifies success
+ */
+int main(void)
+{
+	printf('hello world\n');
+	return (0);
+}
