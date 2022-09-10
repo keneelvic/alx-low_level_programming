0cdd49f8bd529d9fe93b8d2a1bb03ea862de419b
Keneel <keneelsoundton@gmail.com>
2022-09-10 14:03:07 -0700

    a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <sys/syscall.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 1(Error to stderr)
+ */
+int main(void)
+{
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	syscall(SYS_write, 1, msg, sizeof(msg));
+	return (1);
+}
