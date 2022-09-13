a7d0ca9ada8632b87df0d54640b351306de9e9b5
Keneel <keneelsoundton@gmail.com>
2022-09-12 17:50:51 -0700

    program that prints upper and lower case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,11 @@ int main(void)
 	int Alpha_1;
 	int Alpha_2;
 
-	for (Alpha_1 = 'a'; Alpha_1 < 'z'; Alpha_1++)
+	for (Alpha_1 = 'a'; Alpha_1 <= 'z'; Alpha_1++)
 {
 	putchar(Alpha_1);
 }
-	for (Alpha_2 = 'A'; Alpha_2 < 'Z'; Alpha_2++)
+	for (Alpha_2 = 'A'; Alpha_2 <= 'Z'; Alpha_2++)
 {
 	putchar(Alpha_2);
 }
