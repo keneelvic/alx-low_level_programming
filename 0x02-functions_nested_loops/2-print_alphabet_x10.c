698c2db91218e25f136d6b0aa2987942732d74ca
Keneel <keneelsoundton@gmail.com>
2022-09-14 15:16:44 -0700

    c-functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,9 +11,9 @@
 void print_alphabet_x10(void)
 {
 	int num;
-        char alpha;
+	char alpha;
 
-	for (num = 0; num <= 10; num++)
+	for (num = 0; num < 10; num++)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
