35699827ccda2496fa669dd7a43a9b2015a54348
Keneel <keneelsoundton@gmail.com>
2022-09-14 13:59:11 -0700

    c-functions, nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,19 +7,16 @@
  *
  * Return: void
  */
+
 void print_to_98(int n)
 {
 	while (n != 98)
-{
-	 printf("%d, ", n);
-
+	{
+		printf("%i, ", n);
 		if (n > 98)
-	{
-		  n--;
-	} else
-	{
-		n++;
+			n--;
+		else
+			n++;
 	}
-}
 	printf("98\n");
 }
