07e9bea7153ea165703dd3ce47a9ef88d518057d
Keneel <keneelsoundton@gmail.com>
2022-09-14 14:56:59 -0700

    c-functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,15 +7,11 @@
  * Return: 0 or 1
  */
 
+
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z') || ( >= 'A' && c <= 'Z')
-	{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
-
 }
