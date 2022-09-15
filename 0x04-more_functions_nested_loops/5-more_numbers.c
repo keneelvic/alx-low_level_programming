08652532908059d1089f9811f90b01714e87d6db
Keneel <keneelsoundton@gmail.com>
2022-09-15 16:57:40 -0700

    this program creates 10 rows wherein nos from 0 to 14 are printed

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers- this program prints 0 to 14 in 10 rows
+ *
+ * _putchar: used 3 times
+ */
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y >= 10)
+			{
+				_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
