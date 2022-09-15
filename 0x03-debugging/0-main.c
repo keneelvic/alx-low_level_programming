2b9a7bcb0944caad8e128b52a0cc94798aea71fd
Keneel <keneelsoundton@gmail.com>
2022-09-14 19:15:22 -0700

    main file thay tests if a function cn print  negative and positive nos

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main- entry point
- *
- * Return: Always 0
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
+
 int main(void)
 {
 	int i;
