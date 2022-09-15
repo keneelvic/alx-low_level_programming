9c1272d549f49c5361fc2916ca334677f67c51b9
Keneel <keneelsoundton@gmail.com>
2022-09-15 15:41:42 -0700

    this program multiplies two arguments

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *mul - multiplies two arguments
+ *@a: arg one
+ *@b: arg two
+ *
+ * Return: the multplcatrion of the two args
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
