564370adcd6456e4e3873a958d2f707b61be98c8
Keneel <keneelsoundton@gmail.com>
2022-09-15 17:54:01 -0700

    this program prints a straigth line of varyng legnht depending on the number u input

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line based on the number u input
+ *@n: determines the length of the underscore represented with numbers
+ *
+ * _putchar: used 3 times
+ */
+void print_line(int n)
+{
+	int l;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (l = 1; l < n; l++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+
+}
