1ed1effcd4166086677b6b6b113a1b3c55378a9a
Keneel <keneelsoundton@gmail.com>
2022-09-19 13:53:23 -0700

    deferencing n using the pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the value of the variable in the main function to 98
+ * it dereferences the variable
+ *
+ *@n: this is a  pointer of type integer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
