5a1aa505ea19f2b433b4850ab58522fd043303d7
Keneel <keneelsoundton@gmail.com>
2022-09-19 14:08:40 -0700

    swap and dereferencing of values of two integwers using pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - assigns new value to the integers
+ *
+ * @a: the first integer being pointed to
+ * @b: the second integer being pointed to
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
