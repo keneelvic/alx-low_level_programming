485b35950bbcbd3bd43bd1162d2c7805e8106204
Keneel <keneelsoundton@gmail.com>
2022-09-22 14:58:22 -0700

    program that concats strings

--- 0x06-pointers_arrays_strings/0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0x06-pointers_arrays_strings/0-strcat.c
@@ -9,17 +9,12 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i = 0, j = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-
-	}
-	for (j = 0; src[j] != '\0'; j++, i++)
-	{
-		dest[i] = src[j];
-	}
-	dest[i] = 0;
+	while (dest[i++])
+		j++;
+	for (i = 0; src[i]; i++)
+		dest[j++] = src[i];
 
 	return (dest);
 }
