c955b1086b0fa62349356deb15a397fc341e2ea9
Keneel <keneelsoundton@gmail.com>
2022-09-26 15:56:15 -0700

    a function that locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _strstr - prints the consecutive caracters of s1 that are in s2.
+ * @haystack: source string
+ * @needle: searching string
+ *
+ * Return: new string.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		if ((*haystack == *needle && coincidence(haystack, needle) == 1) || !*needle)
+		{
+			return (haystack);
+		}
+		else
+		{
+			haystack++;
+		}
+	}
+	return (0);
+}
+/**
+ *coincidence - define if the string y is inside x.
+ * @x: source string
+ * @y: string to be searched
+ *
+ * Return: 1 if there is coincidence, otherwise 0.
+ */
+
+int coincidence(char *x, char *y)
+{
+	while (*y && *y == *x)
+	{
+		y++;
+		x++;
+	}
+	if (*y == '\0')
+		return (1);
+	else
+		return (0);
+}
+
