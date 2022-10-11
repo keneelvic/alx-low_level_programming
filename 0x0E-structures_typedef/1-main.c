5d35c93b4acf80581d14df58bd67ee84bb876835
Keneel <keneelsoundton@gmail.com>
2022-10-11 02:35:04 -0700

    a function that initializes a a variable of struct dog

---
+++ 0x0E-structures_typedef/1-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	struct dog my_dog;
+
+	init_dog(&my_dog, "Poppy", 3.5, "Bob");
+
+	printf("my name is %s, and I am %.1f :) - Whoof!\n", my_dog.name, my_dog.age);
+	
+	return (0);
+}
