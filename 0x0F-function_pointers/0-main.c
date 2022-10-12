ad0144edd09e0c045318c25ac8c1581ba8f21df3
Keneel <keneelsoundton@gmail.com>
2022-10-12 16:03:00 -0700

    a function that prints a name.

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -42,8 +42,8 @@ void print_name_uppercase(char *name)
  */
 int main(void)
 {
-	 print_name("Bob", print_name_as_is);
-	 print_name("Bob Dylan", print_name_uppercase);
+	 print_name("KENEEL  victor", print_name_as_is);
+	 print_name("kenechukwu okOye", print_name_uppercase);
 	 printf("\n");
 	 return (0);
 }
