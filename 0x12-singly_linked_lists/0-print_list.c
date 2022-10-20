9e8c55267a2e880da783494b55dcab9a448f6a04
Keneel <keneelsoundton@gmail.com>
2022-10-20 15:25:58 -0700

    single-linked lists and its usage

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: the list to print
+ *
+ * Return: the number of element to print.
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int len = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		len++;
+	}
+	return (len);
+}
