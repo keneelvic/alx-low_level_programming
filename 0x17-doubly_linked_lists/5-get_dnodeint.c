585ca96e12fae091bf77448cc6210e36304cd6ce
Keneel <keneelsoundton@gmail.com>
2022-12-12 11:29:01 -0800

    get the node at a particular index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - Return the nth node
+ * @head: Pointer to the head
+ * @index: The node index
+ * Return: The direction of the nth node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count;
+
+	if (head == NULL)
+		return (NULL);
+	for (count = 0; count < index; count++)
+	{
+		head = head->next;
+	}
+	return (head);
+}
